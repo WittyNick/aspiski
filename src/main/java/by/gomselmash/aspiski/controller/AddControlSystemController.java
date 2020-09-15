cb350ffd6258c8bd3a4ecf1f80f6ddffad8f222e
Pavel Kryvasheyeu <p.e.krivosheev@gmail.com>
2020-09-15 16:28:51 +0300

    fix html forms incomplete

--- src/main/java/by/gomselmash/aspiski/controller/AddControlSystemController.java
+++ src/main/java/by/gomselmash/aspiski/controller/AddControlSystemController.java
@@ -1,13 +1,25 @@
 package by.gomselmash.aspiski.controller;
 
+import by.gomselmash.aspiski.model.ControlSystem;
+import by.gomselmash.aspiski.repository.ControlSystemRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class AddControlSystemController {
+    private final ControlSystemRepository controlSystemRepository;
+
+    public AddControlSystemController(ControlSystemRepository controlSystemRepository) {
+        this.controlSystemRepository = controlSystemRepository;
+    }
 
     @GetMapping("/add-control-system")
-    public String goAddControlSystem() {
+    public String goAddControlSystem(Model model) {
+        List<ControlSystem> controlSystems = controlSystemRepository.findAll(); // when nothing was found returns empty List
+        model.addAttribute("controlSystems", controlSystems);
         return "add_control_system";
     }
 }
---
+++ src/main/resources/static/js/add_control_system.js
@@ -0,0 +1,3 @@
+function goWelcomePage() {
+    $(location).prop('href', '/');
+}
